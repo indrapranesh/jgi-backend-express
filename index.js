a4538155dddbb30a31c5ba7f56f74eea8c6367ac
pranesh <indrapranesh2111@gmail.com>
2021-08-13 00:32:08 +0530

    update docusign APIs

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const express = require('express')
   , envelopeController = require('./lib/controllers/envelopeController.js')
   , userController = require('./lib/controllers/docusignUser.js')
   , auditController = require('./lib/controllers/auditController.js')
+  , mapController = require('./lib/controllers/mapController')
 
 // Setup server port
 const port = process.env.PORT || 5000;
 
 const app = express()
-  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
   .use(cors())
-  .use(bodyParser.json())
+  .use(bodyParser.json({limit: '50mb'}))
 
   // Routes
 
@@ -21,11 +22,17 @@ const app = express()
   .post('/group/user/create', userController.createGroupUser)
   .get('/envelope/data/:id', envelopeController.getEnvelopeData)
   .get('/envelope/map/:id', envelopeController.getMapImage)
+  .get('/envelope/comments/:id', envelopeController.getComments)
 
   //Audit
   .get('/audits', auditController.getAllAudits)
   .post('/audit', auditController.createAudit)
+  .post('/review', auditController.createReview)
   .get('/review/envelopes/:id', auditController.getEnvelopesByReview)
+
+  //map
+  .get('/map', mapController.getMap)
+  .patch('/map', mapController.updateVersion)
 
 
 // start server
